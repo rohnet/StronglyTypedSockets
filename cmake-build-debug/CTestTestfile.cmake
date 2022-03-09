d6b709ae5536b96e3fe97749c99a0a46847c2e2f
Danila Kotin <kotin.danila@yandex.ru>
2022-03-09 21:24:59 +0300

    rm build and idea

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniel/Projects/protei_test
-# Build directory: /home/daniel/Projects/protei_test/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
